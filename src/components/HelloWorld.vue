3974e453e62ba4ad4c347421dd597aaf9e7bf056
Mukhutdinov Ruslan <mukhutdinov.ruslan@gmail.com>
2023-01-27 11:36:46 +0500

    edit btn

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -28,11 +28,9 @@
           <v-btn
             class="gtm-button"
             color="primary"
-            href="https://next.vuetifyjs.com/introduction/why-vuetify/#feature-guides"
             min-width="228"
             rel="noopener noreferrer"
             size="x-large"
-            target="_blank"
             variant="flat"
           >
             <v-icon icon="mdi-speedometer" size="large" start />
@@ -42,13 +40,7 @@
         </v-col>
 
         <v-col cols="auto">
-          <v-btn
-            href="https://community.vuetifyjs.com/"
-            min-width="164"
-            rel="noopener noreferrer"
-            target="_blank"
-            variant="text"
-          >
+          <v-btn class="gtm-button" min-width="164" variant="text">
             <v-icon icon="mdi-account-group" size="large" start />
 
             Community
